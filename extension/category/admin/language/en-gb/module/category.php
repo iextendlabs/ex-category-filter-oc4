a33984a29b07908484cc00cf8adf62265cea5667
MZohaib1908 <mzohaib1908@gmail.com>
2025-09-10 22:07:14 +0500

    Category Filter Extension
    Add Filter in Category List To search Categories in the admin side.

---
+++ extension/category/admin/language/en-gb/module/category.php
@@ -0,0 +1,17 @@
+<?php
+// Heading
+$_['heading_title']    = 'Category Module';
+
+// Text
+$_['text_extension']   = 'Extensions';
+$_['text_success']     = 'Success: You have modified Category Module module!';
+$_['text_edit']        = 'Edit Category Module Module';
+
+// Entry
+$_['entry_status']     = 'Status';
+$_['entry_description']     = 'Description';
+$_['entry_link']     = 'Link';
+$_['entry_link_title']     = 'Link Title';
+
+// Error
+$_['error_permission'] = 'Warning: You do not have permission to modify Category Module module!';
